a208dfbab5f5eb7a4690ae9bc2c02a8058237f6a
whale.park18 <whale.park18@gmail.com>
2024-08-27 23:27:16 +0900

    refactor: 솔루션 리팩토링
    <수정 내용>
    - include 솔루션 폴더 추가
        - utils 폴더 이동
    - safe_data 헤더 추가
    - global 헤더(전역 include 헤더) 추가
        - pch 사용 안함는 대신 사용

---
+++ include/global.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "framework.h"
+#include "targetver.h"
+#include "type_define.h"
+#include "safe_data.h"
+#include "utils/error.h"
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <format>
+
+#include <WinSock2.h>
---
+++ include/type_define.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace park18::types
+{
+	using uchar  = unsigned char;
+	using ushort = unsigned short;
+	using uint	 = unsigned int;
+	using ulong  = unsigned long;
+
+	using wchar = wchar_t;
+
+	constexpr uint BufSize = 1024;
+}
